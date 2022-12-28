44841cd3f55999b333cf4b905fda857c2cc7d7fe
mons1201 <mohanmons1201@gmail.com>
2022-12-27 22:54:03 -0800

    validate

---
+++ Arrayform.js
@@ -0,0 +1,49 @@
+let studentvalues=["Mohan","Mohana","Dinesh","Swetha"];
+
+var inserting=()=>
+{
+    let temp=document.getElementById('users')
+    studentvalues.push(temp.value);
+    alert("your value is added successfully"+temp.value)
+}
+
+let viewpoint=()=>
+{
+    let hi="";
+    studentvalues.map((data)=>
+    {
+        hi+="<tr><td>"+data+"</td></tr>"
+    })
+    const performance=document.getElementById('live');
+    performance.innerHTML="<table align='center' style='padding: 10px 10px 5px 10px; box-shadow:5px 6px 4px pink;'><thead><tr><th> Student Names</th></tr></thead><tbody>"+hi+"</tbody></table>"
+}
+
+let updating=()=>
+{
+    let index=document.getElementById('users').value
+    const newone=prompt("Tell us what value you want update/replace"+studentvalues[index])
+    studentvalues[index]=newone;
+}
+
+var deleting=()=>
+{
+    let temp=document.getElementById('users').value;
+    studentvalues=studentvalues.filter((data)=>
+    {
+        return data!==temp;
+    })
+}
+
+var searching=()=>
+{
+    let temp=document.getElementById('users').value;
+    for(let index=0;index<studentvalues.length;index++)
+    {     
+        if(studentvalues[index]===temp)
+        {
+            return index;
+        }
+    }
+    
+   
+}
